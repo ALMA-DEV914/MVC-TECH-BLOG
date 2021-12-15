64053e0253036e8736e50d1ad11518fffb002b04
ALMA_DEV914 <aungonalna58@gamil.com>
2021-12-14 23:16:58 -0800

    add all required routes and packages on main app

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const controllers = require('./controllers');
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+// Set up sessions
+const sess = {
+    secret: "Secret key goes here",
+    cookie: {
+        // Stored in milliseconds (86,400,000 === 1 day)
+        //28800000 = 8 hours
+        maxAge: 28800000,
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+app.use(session(sess));
+// set up handlebars with express
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+//use the middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
 app.use(controllers);
 
