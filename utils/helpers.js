0081940e191750f5f4e312d69b7e75541a0a24b6
ALMA_DEV914 <aungonalna58@gamil.com>
2021-12-15 14:55:09 -0800

    add edit handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,8 @@
 module.exports = {
     // the helper method 'format_time' will take in a timestamp and return a custom formatted string
-    format_time: (date) => {
+    format_time: (date)=> {
         //'toLocaleTimeString()' method to format the time with custom parameters
-        return date.toLocaleDateString("en-US", {
+        return date.toLocalDateString("en-US", {
             year: "numeric",
             month: "long",
             day: "numeric",
