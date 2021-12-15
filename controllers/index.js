f535bf981ba842231f018cd4ec8f22e2bd1ecd47
ALMA_DEV914 <aungonalna58@gamil.com>
2021-12-15 11:24:47 -0800

    add post routes, connect the file path

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,16 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const postRoutes = require('./post-routes.js');
+const signupRoutes = require('./signup-routes.js');
+const loginRoutes = require('./login-routes.js');
+
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/login', loginRoutes);
+router.use('/post', postRoutes);
+router.use('/signup', signupRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
