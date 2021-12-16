7ed289067965d61f085b1c15f81089a4d21cefd2
ALMA_DEV914 <aungonalna58@gamil.com>
2021-12-15 16:03:47 -0800

    add logout routes and javacsript code for function

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,23 @@
 const router = require('express').Router();
 //require all connected file routes 
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
+const homepageRoutes = require('./home-routes.js');
 const postRoutes = require('./post-routes.js');
 const signupRoutes = require('./signup-routes.js');
 const loginRoutes = require('./login-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 const editRoutes = require('./edit-routes.js');
+const logoutRoutes = require('./logout-routes.js');
 
 //callback function to use the routes fora certain file
 router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
+router.use('/', homepageRoutes);
 router.use('/login', loginRoutes);
 router.use('/post', postRoutes);
 router.use('/signup', signupRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/edit', editRoutes);
+router.use('/logout', logoutRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
