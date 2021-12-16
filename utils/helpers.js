2deffd5b33c06b437f92a214bf0d9eeece1d92b1
ALMA_DEV914 <aungonalna58@gamil.com>
2021-12-16 15:54:54 -0800

    add heroku

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,12 @@
+
 module.exports = {
     // the helper method 'format_time' will take in a timestamp and return a custom formatted string
     format_time: (date) => {
         //'toLocaleTimeString()' method to format the time with custom parameters
-        return date.toLocaleDateString("en-US", {
+        if(!date){
+            return "date-unknown";
+        }
+        return date.toLocaleDateString('en-Us', {
             year: "numeric",
             month: "long",
             day: "numeric",
@@ -19,3 +23,4 @@ module.exports = {
         }
     },
 };
+
